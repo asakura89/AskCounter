42a50480eb207b14e9778e5b293a104590d52785
asakura89 <flamboyan3no1a@gmail.com>
2018-12-20 20:12:53 +0700

    initial commit

---
+++ AskCounter/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+namespace AskCounter
+{
+    public partial class MainWindow : Window
+    {
+        Int32 internalCounter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDown_Click(object sender, RoutedEventArgs e)
+        {
+            if (--internalCounter <= 0)
+                internalCounter = 0;
+
+            UpdateUI();
+        }
+
+        private void BtnUp_Click(object sender, RoutedEventArgs e)
+        {
+            ++internalCounter;
+            UpdateUI();
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            internalCounter = 0;
+            UpdateUI();
+        }
+
+        void UpdateUI()
+        {
+            TxtCounter.Text = internalCounter.ToString();
+        }
+    }
+}
